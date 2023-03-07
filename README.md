4762365cb5842d418d7fd22443facabda4809299
onkar-date <onkar101197@gmail.com>
2023-03-08 00:57:05 +0530

    README.md Updated

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Chess
+
 Simple JavasSript code which identifies possible moves of chess piece.
+
+# Steps to run project
+
+1. Set the the piece type and current position values
+2. in Terminal run command node main.js
+3. Code will get executed and outpur will be printed in console
+4. Change the piece type and cell position to check different possible moves
--- main.js
+++ main.js
@@ -3,4 +3,11 @@ import { PIECE_TYPE, CELLS } from "./constants/chessConstants.js";
 
 const chessBoard = new Chessboard();
 
-console.log(chessBoard.getPossibleMoves(PIECE_TYPE.QUEEN, CELLS.G3));
+// Set below values
+const pieceType = PIECE_TYPE.KING;
+const currentPosition = CELLS.G3;
+
+// Possible Moves will be calculated
+const possibleMoves = chessBoard.getPossibleMoves(pieceType, currentPosition);
+
+console.log(possibleMoves);
